95d43e47fdbe326906202d7465fcc8da09c187a8
seattleowl <32573897+seattleowl@users.noreply.github.com>
2022-06-27 18:17:53 -0700

    all the things

--- renderer.js
+++ renderer.js
@@ -10,8 +10,18 @@ const imageOperator = imageOperatorEl.getContext("2d");
 const frameList = document.querySelector("div#frame-list");
 /** @type {HTMLSpanElement} */
 const playButton = document.querySelector("span#play-button");
+/** @type {HTMLSpanElement} */
+const trashButton = document.querySelector("span#trash-button");
+/** @type {HTMLSpanElement} */
+const videoSettingsButton = document.querySelector(
+	"span#video-settings-button"
+);
 
-const addFrame = (dataUrl) => {
+let selectedFrame = -1;
+let playInterval = null;
+let fps = 5;
+
+const addFrame = (dataUrl, index = frameList.children.length - 1) => {
 	const img = document.createElement("img");
 	img.src = dataUrl;
 	img.classList.add("frame");
@@ -30,6 +40,8 @@ const addFrame = (dataUrl) => {
 
 		cameraFeed.classList.add("hidden");
 		captureBtn.disabled = true;
+		trashButton.innerText = "delete";
+		selectedFrame = index;
 
 		const ratio = Math.max(streamWidth / width, streamHeight / height);
 		const [drawWidth, drawHeight] = [streamWidth / ratio, streamHeight / ratio];
@@ -43,7 +55,7 @@ const addFrame = (dataUrl) => {
 		imageOperator.drawImage(img, offsetX, offsetY, drawWidth, drawHeight);
 	});
 
-	frameList.insertBefore(img, frameList.querySelector("div.frame.camera"));
+	frameList.insertBefore(img, frameList.children[index]);
 };
 
 const selectCamera = () => {
@@ -52,6 +64,81 @@ const selectCamera = () => {
 		.forEach((el) => el.classList.remove("active"));
 	cameraFeed.classList.remove("hidden");
 	captureBtn.disabled = false;
+	trashButton.innerText = "undo";
+	selectedFrame = -1;
+};
+
+const downloadURI = (uri, name) => {
+	console.log(uri);
+	const link = document.createElement("a");
+	link.download = name;
+	link.href = uri;
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+};
+
+const exportVideo = () => {
+	const { width, height } = cameraFeed.getBoundingClientRect();
+	const { width: streamWidth, height: streamHeight } = cameraStream
+		.getTracks()[0]
+		.getSettings();
+	imageOperatorEl.width = width;
+	imageOperatorEl.height = height;
+	imageOperator.clearRect(0, 0, width, height);
+
+	const ratio = Math.max(streamWidth / width, streamHeight / height);
+	const [drawWidth, drawHeight] = [streamWidth / ratio, streamHeight / ratio];
+	const [offsetX, offsetY] = [
+		(width - drawWidth) / 2,
+		(height - drawHeight) / 2
+	];
+
+	cameraFeed.classList.add("hidden");
+	captureBtn.disabled = true;
+	playButton.innerText = "downloading";
+
+	const videoStream = imageOperatorEl.captureStream(0);
+	const mediaRecorder = new MediaRecorder(videoStream);
+
+	let chunks = [];
+	mediaRecorder.addEventListener("dataavailable", (e) => {
+		chunks.push(e.data);
+	});
+	mediaRecorder.addEventListener("stop", () => {
+		const blob = new Blob(chunks, { type: "video/mp4" }); // other types are available such as 'video/webm' for instance, see the doc for more info
+		chunks = [];
+		const url = URL.createObjectURL(blob);
+		console.log(url);
+		downloadURI(url, "my-video.mp4");
+	});
+
+	let frame = 0;
+	mediaRecorder.start();
+	playInterval = setInterval(() => {
+		// imageOperator.clearRect(0, 0, width, height);
+		if (frameList.children[frame] == null) {
+			clearInterval(playInterval);
+			mediaRecorder.stop();
+			playButton.innerText = "play_arrow";
+			selectCamera();
+			return;
+		} else if (frameList.children[frame].classList.contains("camera")) {
+			frame++;
+			return;
+		}
+
+		imageOperator.drawImage(
+			frameList.children[frame],
+			offsetX,
+			offsetY,
+			drawWidth,
+			drawHeight
+		);
+		frame++;
+
+		videoStream.getVideoTracks()[0].requestFrame();
+	}, 1000 / fps);
 };
 
 let capturing = false;
@@ -105,6 +192,13 @@ captureBtn.addEventListener("click", () => {
 });
 
 playButton.addEventListener("click", () => {
+	if (playButton.innerText === "pause") {
+		clearInterval(playInterval);
+		selectCamera();
+		playButton.innerText = "play_arrow";
+		return;
+	} else if (playButton.innerText !== "play_arrow") return;
+
 	const { width, height } = cameraFeed.getBoundingClientRect();
 	const { width: streamWidth, height: streamHeight } = cameraStream
 		.getTracks()[0]
@@ -122,12 +216,14 @@ playButton.addEventListener("click", () => {
 
 	cameraFeed.classList.add("hidden");
 	captureBtn.disabled = true;
+	playButton.innerText = "pause";
 
 	let frame = 0;
-	const interval = setInterval(() => {
+	playInterval = setInterval(() => {
 		// imageOperator.clearRect(0, 0, width, height);
 		if (frameList.children[frame] == null) {
-			clearInterval(interval);
+			clearInterval(playInterval);
+			playButton.innerText = "play_arrow";
 			selectCamera();
 			return;
 		} else if (frameList.children[frame].classList.contains("camera")) {
@@ -143,5 +239,38 @@ playButton.addEventListener("click", () => {
 			drawHeight
 		);
 		frame++;
-	}, 200);
+	}, 1000 / fps);
+});
+
+trashButton.addEventListener("click", () => {
+	const index =
+		selectedFrame === -1 ? frameList.children.length - 2 : selectedFrame;
+
+	if (index >= 0) frameList.children[index].remove();
+	if (selectedFrame !== -1) selectCamera();
+});
+
+videoSettingsButton.addEventListener("click", () => {
+	const settingsWindow = open(
+		"settings.html",
+		"_blank",
+		"width=700,height=500,left=100,top=100"
+	);
+
+	console.log("sending");
+
+	addEventListener("message", (event) => {
+		if (event.origin !== `${location.protocol}//${location.host}`) {
+			console.log(event.origin);
+			return;
+		}
+
+		if (event.data.action === "pref") {
+			fps = event.data.fps;
+		} else exportVideo();
+	});
+
+	settingsWindow.addEventListener("load", () =>
+		settingsWindow.postMessage({ fps })
+	);
 });
